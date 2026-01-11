80d1224a3dd21b3693ae35af8110af44fe81d895
Nick24s <nickmilenov@gmail.com>
2026-01-11 12:52:50 +0200

    add submit from keyboard

--- App.js
+++ App.js
@@ -34,6 +34,7 @@ export default function App() {
             placeholder="go to the gym!"
             value={text}
             onChangeText={textChangeHandler}
+            onSubmitEditing={createTodoHandler}
             />
             <Button title="Create" onPress={createTodoHandler}/>
         </View>
